b4b6f4e0bb18d5210fb98b64538a33c6b14b5ea7
Mark Gibson <mgibson@adaptavist.com>
2023-07-07 12:16:10 +0100

    Ensure the blog really is lazy

--- handler.ts
+++ handler.ts
@@ -3,8 +3,8 @@ import { staticRoute } from "$http_fns/static.ts";
 import { byPattern } from "$http_fns/pattern.ts";
 import { interceptResponse, skip } from "$http_fns/intercept.ts";
 import routes from "@/routes.ts";
-import blogRoute from "@/routes/blog/:path*.tsx";
 import oauth2 from "@/handlers/oauth2.ts";
+import { lazy } from "$http_fns/lazy.ts";
 
 export default handle([
   interceptResponse(
@@ -12,7 +12,10 @@ export default handle([
     skip(404),
   ),
   oauth2,
-  byPattern("/blog/:path*{.:ext}", blogRoute),
+  byPattern(
+    "/blog/:path*{.:ext}",
+    lazy(() => import("@/routes/blog/:path*.tsx")),
+  ),
   routes,
   staticRoute("/", import.meta.resolve("@/static")),
 ]);
