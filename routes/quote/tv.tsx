542e303367255fa23e966437e05da28ee0a719bf
Mark Gibson <mgibson@adaptavist.com>
2024-03-14 16:08:12 +0000

    Disable deferred streaming for quote tv page

--- routes/quote/tv.tsx
+++ routes/quote/tv.tsx
@@ -1,26 +1,29 @@
-import { Page } from "../../components/Page.tsx";
-import { renderPage } from "../../lib/handle_page.ts";
 import { byMethod } from "@http/fns/by_method";
 import { Quote } from "./_components/Quote.tsx";
-import { getSearchValues } from "jsr:@http/fns@^0.6.3/request/search_values";
+import { renderHTML } from "../../lib/render_html.tsx";
 
 export default byMethod({
-  GET: (req, match: URLPatternResult) => {
-    const refresh = getSearchValues(req)("refresh")[0] ??
-      Deno.env.get("QUOTE_TV_REFRESH") ?? "420";
-    return renderPage(() => (
-      <html lang="en-GB">
-        <head>
-          <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta name="refresh" content={refresh} />
-          <link rel="stylesheet" href="tv.css" />
-          <script src="tv.js" defer />
-        </head>
-        <body>
-          <Quote />
-        </body>
-      </html>
-    ))(req, match);
+  GET: (req) => {
+    const refresh = new URL(req.url).searchParams.get("refresh") ??
+      Deno.env.get("QUOTE_TV_REFRESH");
+
+    return renderHTML(QuoteTvPage, { refresh });
   },
 });
+
+function QuoteTvPage({ refresh }: { refresh?: string | number }) {
+  return (
+    <html lang="en-GB">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="refresh" content={refresh ?? 420} />
+        <link rel="stylesheet" href="tv.css" />
+        <script src="tv.js" defer />
+      </head>
+      <body>
+        <Quote />
+      </body>
+    </html>
+  );
+}
